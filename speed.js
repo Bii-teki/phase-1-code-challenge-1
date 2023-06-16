a61fdcd7a65a0f0474e5b70e4fdbb34d81706fe8
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-16 17:21:09 +0300

    update on marks error entry and speed file

--- speed.js
+++ speed.js
@@ -1,15 +1,28 @@
-checkSpeed(130);
 
-function checkSpeed(speed) {
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['speed'], function (err, result) 
+
+
+ {
   const speedLimit = 70;
   const kmPerPoint = 5;
-
-  if (speed < speedLimit + kmPerPoint) {
+  if (err) { return onErr(err);
+  }
+  if (result.speed < speedLimit + kmPerPoint) {
     console.log("Ok");
     return;
   }
 
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  const points = Math.floor((result.speed - speedLimit) / kmPerPoint);
   if (points >= 12) console.log("License suspended");
   else console.log("Points", points);
+});
+
+
+function onErr(err) {
+  console.log(err);
+  return 1;
 }
